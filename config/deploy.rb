f9c9101175ed5bebd729ae516d116eaa9ae968f0
webwerks <webwerks@webwerks-G31M-ES2L.(none)>
2012-09-14 19:33:08 +0530

    changes in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 require 'bundler/capistrano'
 set :application, "college"
-
 set :use_sudo, false
 
 set :scm, :git
@@ -8,21 +7,20 @@ set :repository,  "https://github.com/mangocoder/college.git"
 set :scm_passphrase, "madyrocks"
 
 set :user, "root"
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "180.149.241.115"                          # Your HTTP server, Apache/etc
-role :app, "180.149.241.115"                          # This may be the same as your `Web` server
-role :db,  "180.149.241.115", :primary => true # This is where Rails migrations will run
+role :web, "localhost"                          # Your HTTP server, Apache/etc
+role :app, "localhost"                          # This may be the same as your `Web` server
+role :db,  "localhost", :primary => true # This is where Rails migrations will run
+
 #role :db,  "your slave db-server here"
 
 set :deploy_to, "/var/www/college"
 set :keep_releases, 3 
 set :server, :passenger
 after "deploy", "deploy:production"
-after "deploy", "deploy:migrate"
-after "deploy", "deploy:cleanup"
-#after "deploy:update_code", "init:bundle_install"
+
 after "deploy:update_code", "init:create_db"
+after  "init:create_db", "deploy:migrate" 
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -38,10 +36,8 @@ after "deploy:update_code", "init:create_db"
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
    task :production do
-     # this is the default, but left here for reference
      set :bundle_without, [:development, :test]
-     # other production specific settings, like
-      set :rails_env, 'production'
+     set :rails_env, 'production'
    end	  
  end
 
